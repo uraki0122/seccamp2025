3ac2560e237e106e020da464bec3610bb756be9b
Uraki <m22026@g.metro-cit.ac.jp>
2025-08-12 17:48:28 +0900

    a

--- obfuscated_sample.py
+++ obfuscated_sample.py
@@ -1,27 +1,60 @@
-def KAWxGQFK(data):
-    return ''.join((chr(c - 5) for c in data))
+def eVbhLKer(data):
+    return ''.join((chr(c - 6) for c in data))
 
-def UcasqMAE():
-    ZRluIlvX = 0
+class MyClass:
+
+    def __init__(self, value):
+        self.value = value
+
+    def sZErkyot(self):
+        jjZHXRII = 0
+        while True:
+            if jjZHXRII == 0:
+                print(f'Value is {self.value}')
+                jjZHXRII = 1
+            if jjZHXRII == 1:
+                break
+
+    def __str__(self):
+        return f'MyClass with value {self.value}'
+
+def ojDUurDB():
+    kEzMoGCN = 0
     while True:
-        if ZRluIlvX == 0:
-            print(KAWxGQFK([960 ^ 909, 82 + 24, 802 ^ 851, 326 ^ 311, 94 + 22, 26 + 23, 455 ^ 482, 85 + 7, 336 ^ 292, 637 ^ 522, 426 ^ 475, 680 ^ 705, 19 + 19]))
-            ZRluIlvX = 1
-        if ZRluIlvX == 1:
+        if kEzMoGCN == 0:
+            print(eVbhLKer([19 + 59, 58 + 49, 98 + 16, 61 + 53, 70 + 47, 37 + 13, 1 + 37, 329 ^ 308, 45 + 72, 121 ^ 1, 80 + 34, 704 ^ 682, 305 ^ 278]))
+            kEzMoGCN = 1
+        if kEzMoGCN == 1:
             break
 
-def keRMiqij():
-    mHLeARGS = 0
+def tlfaVwOi():
+    obj = MyClass(16 + 26)
+    nuohtlbb = 0
     while True:
-        if mHLeARGS == 0:
-            UcasqMAE()
-            mHLeARGS = 1
-        if mHLeARGS == 1:
+        if nuohtlbb == 0:
+            obj.normal_method()
+            nuohtlbb = 1
+        if nuohtlbb == 1:
             break
-AdvyKsEF = 0
-while True:
-    if AdvyKsEF == 0:
-        keRMiqij()
-        AdvyKsEF = 1
-    if AdvyKsEF == 1:
-        break
+    agbOrpNg = 0
+    while True:
+        if agbOrpNg == 0:
+            print(obj)
+            agbOrpNg = 1
+        if agbOrpNg == 1:
+            break
+    VMrXVEfq = 0
+    while True:
+        if VMrXVEfq == 0:
+            ojDUurDB()
+            VMrXVEfq = 1
+        if VMrXVEfq == 1:
+            break
+if __name__ == eVbhLKer([63 + 38, 97 + 4, 19 + 96, 37 + 66, 903 ^ 1000, 261 ^ 369, 732 ^ 697, 59 ^ 94]):
+    rmrEqPdq = 0
+    while True:
+        if rmrEqPdq == 0:
+            tlfaVwOi()
+            rmrEqPdq = 1
+        if rmrEqPdq == 1:
+            break
--- sample.py
+++ sample.py
@@ -1,7 +1,21 @@
-def hello_world():
-    print("Hello, World!")
+class MyClass:
+    def __init__(self, value):
+        self.value = value
 
-def main():
-    hello_world()
+    def normal_method(self):
+        print(f"Value is {self.value}")
 
-main()
+    def __str__(self):
+        return f"MyClass with value {self.value}"
+
+def hello():
+    print("Hello, world!")
+#^^
+def hoge():
+    obj = MyClass(42)
+    obj.normal_method()
+    print(obj)
+    hello()
+
+if __name__ == "__main__":
+    hoge()
